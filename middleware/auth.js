137f41b9612926f293e3744f0e05e6bf6f7d50a8
fis12cds <schaffhauserfilip@gmail.com>
2021-11-05 17:03:53 +0800

    finished 14

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 module.exports = function (req, res, next) {
   // Get token from header
