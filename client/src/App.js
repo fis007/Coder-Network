074395435fe87bd0d2c451b61836fa66ceb8c68f
fis12cds <schaffhauserfilip@gmail.com>
2021-11-16 12:22:52 +0800

    finished 34

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
@@ -13,11 +13,7 @@ const App = () => (
       <Routes>
         <Route exact path='/' element={<Landing />} />
 
-        <Route
-          exact
-          path='/register'
-          element={<Register />}
-        />
+        <Route exact path='/register' element={<Register />} />
         <Route className='container' exact path='/login' element={<Login />} />
       </Routes>
     </Fragment>
