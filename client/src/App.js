abb697725952a840f826c88a445818971156c38d
fis12cds <schaffhauserfilip@gmail.com>
2021-11-18 15:10:26 +0800

    finished 45

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 import './App.css';
 
 // Redux
@@ -31,6 +33,11 @@ const App = () => {
         <Routes>
           <Route exact path='/' element={<Landing />} />
           <Route exact path='/register' element={<Register />} />
+
+          <Route
+            path='dashboard'
+            element={<PrivateRoute component={Dashboard} />}
+          />
           <Route
             className='container'
             exact
