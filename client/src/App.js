e51d4061a1c3839f31535e3e0b172ac1f18fa6d7
fis12cds <schaffhauserfilip@gmail.com>
2021-11-22 13:51:25 +0800

    finished 47

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import './App.css';
 
 // Redux
@@ -43,6 +44,10 @@ const App = () => {
           <Route
             path='dashboard'
             element={<PrivateRoute component={Dashboard} />}
+          />
+          <Route
+            path='/create-profile'
+            element={<PrivateRoute component={CreateProfile} />}
           />
         </Routes>
       </Router>
